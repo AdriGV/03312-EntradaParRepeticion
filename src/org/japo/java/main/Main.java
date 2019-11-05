c9d11189e8d8a5f221f560ef542fb03be360d59c
AdriGV <adrian.gimeno.vigo@gmail.com>
2019-11-05 22:28:11 +0100

    :tada: Git initial

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,62 @@
+/* 
+ * Copyright 2019 AdrianGV  adrian.gimeno.alum@iescamp.es.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author AdrianGV adrian.gimeno.alum@iescamp.es
+ */
+public final class Main {
+
+    public static final Scanner SCN
+            = new Scanner(System.in, "Windows-1252")
+                    .useLocale(Locale.ENGLISH).useDelimiter("\\s+");
+
+    public static void main(String[] args) {
+
+        int num;
+
+        //Control
+        boolean control = true;
+
+        do {
+
+            try {
+                
+                System.out.print("Introduzca un número par ...: ");
+                num = SCN.nextInt();
+                boolean condicion = num % 2 == 0;
+
+                if (condicion) {
+
+                    System.out.printf("Número par ...: %d%n", num);
+                    control = false;
+                }
+                
+            } catch (Exception e) {
+                System.out.println("Error");
+
+            } finally {
+                SCN.nextLine();
+            }
+            
+        } while (control);
+
+    }
+}
